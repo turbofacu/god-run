2985e8d81ff223ccb166e67b83d15ac5355119b2
Facundo Pepe <facundopepe@gmail.com>
2020-07-08 23:52:54 -0300

    maybe now

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
-const assetPrefix = isProd ? '/turbofacu.github.io/god-run/' : ''
+const assetPrefix = isProd ? '/god-run' : ''
 
 module.exports = {
   exportPathMap: () => ({
